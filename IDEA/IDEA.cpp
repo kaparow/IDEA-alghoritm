04ec6a610bd0feae4f6dcb4d049944575fbc7627
kaparow <152067276+kaparow@users.noreply.github.com>
2023-12-21 03:36:11 +0300

    upd

---
+++ IDEA/IDEA.cpp
@@ -0,0 +1,39 @@
+﻿// Proiect_SC.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "IDEA.h"
+#include <string>
+#include <iomanip>
+
+
+
+string IDEA_EncryptString(const std::string& input) {
+    IDEA ideatest = IDEA();
+    ideatest.setMessage(input);
+    ideatest.setKey("ZZZZefghijklmnop");
+    string ideaencr = ideatest.encrypt();
+    return ideaencr;
+}
+string IDEA_DecryptString(const std::string& input) {
+    IDEA ideatest = IDEA();
+    ideatest.setMessage(input);
+    ideatest.setKey("ZZZZefghijklmnop");
+    string ideadecr = ideatest.decrypt(input);
+    return ideadecr;
+}
+
+
+int main() {
+    std::cout << "Введите сообщение: ";
+    string input;
+    getline(cin, input);
+    std::cout << "Original: " << input << std::endl;
+    std::string encryptedString = IDEA_EncryptString(input);
+    std::cout << "Encrypted: " << encryptedString << std::endl;
+    std::string decryptedString = IDEA_DecryptString(encryptedString);
+    std::cout << "Decrypted: " << decryptedString << std::endl;
+    return 0;
+    //Only инглиш
+
+}
